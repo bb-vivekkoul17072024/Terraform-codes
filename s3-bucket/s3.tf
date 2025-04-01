7b04af8b008dba35bbf620d1b046ef3f4b6e0ad6
Vivek Koul <vivek.koul@bigbasket.com>
2025-04-01 12:15:10 +0530

    Add Terraform configuration for S3 bucket

---
+++ s3-bucket/s3.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws" #taken from terraform registery
+      version = "5.92.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"  # Set the region to ap-south-1 (Mumbai)
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "vivekkoulterformbucket"  # The unique name of the S3 bucket
+}
